7c3a17482315d7bc67bad4e105b214210f00178c
Bahnas <bahnas114@gmail.com>
2026-02-21 00:51:03 +0200

    Refactor maxSubArray function to simplify logic and improve readability

--- week 1/day_4/maximum_subarray.dart
+++ week 1/day_4/maximum_subarray.dart
@@ -1,21 +1,18 @@
-import 'dart:math';
-
 class Solution {
   int maxSubArray(List<int> nums) {
-    int l = 0;
-    int sum = 0;
-    int cur_sum = 0;
+    int max_sum = nums[0]; // -1 , 1, 1 , 4  ,4 , 5 , 6 ,6 , 6
+    int cur_sum = nums[0];
     //[-2,1,-3,4,-1,2,1,-5,4]
-    for (var r = 0; r < nums.length; r++) {
-      if (r == 0) {
+
+    for (var r = 1; r < nums.length; r++) {
+      cur_sum += nums[r]; // -1 => 1=> -2 => 2 ,4 => 3 => 5 => 6 => 1 => 4
+      if (nums[r] > cur_sum) {
         cur_sum = nums[r];
       }
-      if (nums[r]>nums[l] && nums[r]>sum) {
-        
+      if (cur_sum > max_sum) {
+        max_sum = cur_sum;
       }
-      sum += nums[r];
-      cur_sum = max(cur_sum, sum);
     }
-    return 1;
+    return max_sum;
   }
 }
