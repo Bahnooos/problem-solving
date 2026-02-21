a3d1dd80a2a26b8848417febbf2f92680ea1378f
Bahnas <bahnas114@gmail.com>
2026-02-21 22:53:29 +0200

    Fix twoSum function logic to correctly return indices of numbers that add up to the target

--- week 1/day_5_review/two_sum_II.dart
+++ week 1/day_5_review/two_sum_II.dart
@@ -2,13 +2,15 @@ class Solution {
   List<int> twoSum(List<int> numbers, int target) {
     int left = 0;
     int right = numbers.length - 1;
-    while (numbers[left] + numbers[right] != target) {
-      if (numbers[left] + numbers[right] > target) {
+    while (left < right) {
+      if (numbers[left] + numbers[right] == target) {
+        return [left + 1, right + 1];
+      } else if (numbers[left] + numbers[right] > target) {
         right--;
       } else {
         left++;
       }
     }
-    return [left,right];
+    return [];
   }
 }
