2d0d812077ab6a9dab7f7602882543ad86c91154
Bahnas <bahnas114@gmail.com>
2026-02-23 17:46:00 +0200

    .

--- week 1/day_7/valid_parentheses.dart
+++ week 1/day_7/valid_parentheses.dart
@@ -5,7 +5,7 @@ class Solution {
     for (var i = 0; i < s.length; i++) {
       final String char = s[i];
       if (pairs.containsValue(char)) {
-        stack.add(s[i]);
+        stack.add(char);
       } else {
         if (stack.isEmpty) return false;
         final pooped = stack.last;
