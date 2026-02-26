53b352cd35d9e51dee9f4e9f5df32a169fc86621
Bahnas <bahnas114@gmail.com>
2026-02-27 01:26:12 +0200

    lengthOfLongestSubstring

--- week 2/day_2/Longest Substring Without Repeating Characters.dart
+++ week 2/day_2/Longest Substring Without Repeating Characters.dart
@@ -1,5 +1,18 @@
+import 'dart:math';
+
 class Solution {
   int lengthOfLongestSubstring(String s) {
-    
+    Set<String> window = {};
+    int left = 0;
+    int maxfreq = 0;
+    for (var right = 0; right < s.length; right++) {
+      while (window.contains(s[right])) {
+        window.remove(s[left]);
+        left++;
+      }
+      window.add(s[right]);
+      maxfreq = max(maxfreq, right - left + 1);
+    }
+    return maxfreq;
   }
 }
