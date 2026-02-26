cdb7a3b74e03a0088433602ec220d3ced873e0e6
Bahnas <bahnas114@gmail.com>
2026-02-27 01:27:41 +0200

    add maxLength

--- week 2/day_2/Longest Substring Without Repeating Characters.dart
+++ week 2/day_2/Longest Substring Without Repeating Characters.dart
@@ -4,15 +4,15 @@ class Solution {
   int lengthOfLongestSubstring(String s) {
     Set<String> window = {};
     int left = 0;
-    int maxfreq = 0;
+    int maxLength = 0;
     for (var right = 0; right < s.length; right++) {
       while (window.contains(s[right])) {
         window.remove(s[left]);
         left++;
       }
       window.add(s[right]);
-      maxfreq = max(maxfreq, right - left + 1);
+      maxLength = max(maxLength, right - left + 1);
     }
-    return maxfreq;
+    return maxLength;
   }
 }
