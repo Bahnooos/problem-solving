16a4e4cd7342cd041466c3187f07acdcb7e1b31f
Bahnas <bahnas114@gmail.com>
2026-02-27 23:35:29 +0200

    solveing of binary search

--- week 2/day_3/Binary Search.dart
+++ week 2/day_3/Binary Search.dart
@@ -1,5 +1,22 @@
 class Solution {
   int search(List<int> nums, int target) {
-    
+    int left = 0;
+    int right = nums.length - 1;
+    int mid = (right + left) ~/ 2;
+    // [-1,0,3,5,9,12]
+    while (left <= right) {
+      if (nums[mid] == target) {
+        return mid;
+      }
+      if (nums[mid] < target) {
+        left++;
+        mid = (right + left) ~/ 2;
+      } else {
+        right--;
+        mid = (right + left) ~/ 2;
+      }
+    }
+
+    return -1;
   }
 }
