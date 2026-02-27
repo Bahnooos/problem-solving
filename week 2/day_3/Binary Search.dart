fae24bb03195bfe454f6486fb1682377171a4648
Bahnas <bahnas114@gmail.com>
2026-02-27 23:40:31 +0200

    the new fix

--- week 2/day_3/Binary Search.dart
+++ week 2/day_3/Binary Search.dart
@@ -2,18 +2,17 @@ class Solution {
   int search(List<int> nums, int target) {
     int left = 0;
     int right = nums.length - 1;
-    int mid = (right + left) ~/ 2;
+
     // [-1,0,3,5,9,12]
     while (left <= right) {
+      int mid = (right + left) ~/ 2;
       if (nums[mid] == target) {
         return mid;
       }
       if (nums[mid] < target) {
-        left++;
-        mid = (right + left) ~/ 2;
+        left = mid + 1;
       } else {
-        right--;
-        mid = (right + left) ~/ 2;
+        right = mid - 1;
       }
     }
 
